7ab6beac674a4bfa31016ea829f843b95f3761b6
Flábio Fagundes <flabio.fagundes@gmail.com>
2017-05-30 17:50:40 -0300

    questao 2

---
+++ q2.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int numero;
+    int posicoes[4];
+    char palavra[20];
+    char palavras[4][20];
+    
+    for(int i=0; i<4; i++){
+        scanf("%d", numero);
+        posicoes[i] = numero;
+    }
+    
+    for(int i=0; i<4; i++){
+        scanf("%s", palavra);
+        posicoes[i] = numero;
+    }
+}
