2e1337a95597699f4631880ffca0c17c2cb77167
Flábio Fagundes <flabio.fagundes@gmail.com>
2017-05-30 22:16:39 -0300

    atualizando q2

--- q2.c
+++ q2.c
@@ -1,17 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-    int numero;
+    int i;
     int posicoes[4];
-    char palavra[20];
     char palavras[4][20];
-    
-    for(int i=0; i<4; i++){
-        scanf("%d", numero);
-        posicoes[i] = numero;
-    }
-    
-    for(int i=0; i<4; i++){
-        scanf("%s", palavra);
-        posicoes[i] = numero;
-    }
+    char senha[4];
+
+    for(i=0; i<4; i++)
+        scanf("%d", &posicoes[i]);
+
+    for(i=0; i<4; i++)
+        scanf("%s", palavras[i]);
+
+    for(i=0; i<4; i++)
+        senha[i] = palavras[i][posicoes[i]];
+
+    printf("%s", senha);
+
+    return 0;
 }
